2b9041d931c12df46ceaa7f2ac6a30a9c98ee863
Frederik Rietdijk <fridh@fridh.nl>
2013-11-27 11:28:05 +0100

    Converted room package to a module.

--- unittest/test_room.py
+++ unittest/test_room.py
@@ -1,8 +1,12 @@
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
 
-from acoustics.room.reverberation import (t60_sabine, t60_eyring, t60_millington,
+from acoustics.room import (t60_sabine, t60_eyring, t60_millington,
                                    t60_fitzroy, t60_arau)
+
+from acoustics.room import mean_alpha, nrc
+
+
 
 
 def setup_module(reverberation):
@@ -89,3 +93,48 @@ def test_t60_arau_bands():
 
 def teardown_module(reverberation):
     pass
+
+
+
+def test_mean_alpha_float():
+    alpha = 0.1
+    surface = 10
+    calculated = mean_alpha(alpha, surface)
+    real = 0.1
+    assert_almost_equal(calculated, real)
+
+
+def test_mean_alpha_1d():
+    alpha = np.array([0.1, 0.2, 0.3])
+    surfaces = np.array([20, 30, 40])
+    calculated = mean_alpha(alpha, surfaces)
+    real = 0.222222222
+    assert_almost_equal(calculated, real)
+
+
+def test_mean_alpha_bands():
+    alpha = np.array([[0.1, 0.1, 0.1], [0.2, 0.2, 0.2], [0.3, 0.3, 0.3]])
+    surfaces = np.array([20, 30, 40])
+    calculated = mean_alpha(alpha, surfaces)
+    real = np.array([0.222222222, 0.222222222, 0.222222222])
+    assert_array_almost_equal(calculated, real)
+
+
+def test_nrc_1d():
+    alpha = np.array([0.1, 0.25, 0.5, 0.9])
+    calculated = nrc(alpha)
+    real = 0.4375
+    assert_almost_equal(calculated, real)
+
+
+def test_nrc_2d():
+    alphas = np.array([[0.1, 0.2, 0.3, 0.4], [0.4, 0.5, 0.6, 0.7]])
+    calculated = nrc(alphas)
+    real = np.array([0.25, 0.55])
+    assert_array_almost_equal(calculated, real)
+
+
+
+
+
+
