4c6799a73857fa6c542bc772d43bb9bca9620f94
Felipe <felipeacsi@gmail.com>
2014-03-18 11:30:08 -0300

    data_path() + 'filename.ext'

--- unittest/get_data_path.py
+++ unittest/get_data_path.py
@@ -1,6 +1,6 @@
 import os
 
 
-def data_path(file_name):
+def data_path():
     parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(parent, 'data', file_name)
+    return os.path.join(parent, 'data/')
