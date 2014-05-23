6e8b76283aa9a8c9580122e9714861b185ca49de
Frederik Rietdijk <fridh@fridh.nl>
2014-05-23 19:21:24 +0200

    IEC 61672: Time-averaging and time-weighting

--- acoustics/standards/__init__.py
+++ acoustics/standards/__init__.py
@@ -1,1 +1,21 @@
-from acoustics.standards import iso_tr_25417_2007
+"""
+Standards
+=========
+
+The standards module provides implementations of standards.
+
+.. toctree::
+    :maxdepth: 2
+
+    
+.. automodule:: acoustics.standards.iso_tr_25417_2007
+    :show-inheritance:
+    :members:
+
+
+.. automodule:: acoustics.standards.iec_61672_1_2013
+    :show-inheritance:
+    :members:
+
+"""
+from acoustics.standards import iso_tr_25417_2007, iec_61672_1_2013
