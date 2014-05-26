ae01ac19851840ff8a7eef8184e1e0db92256adc
Frederik Rietdijk <fridh@fridh.nl>
2014-05-26 17:37:15 +0200

    ENH: ISO 9613-1 is now in a seperate module.

--- acoustics/standards/__init__.py
+++ acoustics/standards/__init__.py
@@ -9,13 +9,13 @@ The standards module provides implementations of standards.
 
     
 .. automodule:: acoustics.standards.iso_tr_25417_2007
-    :show-inheritance:
-    
 
+.. automodule:: acoustics.standards.iso_9613_1_1993
 
 .. automodule:: acoustics.standards.iec_61672_1_2013
-    :show-inheritance:
-    
 
 """
-from acoustics.standards import iso_tr_25417_2007, iec_61672_1_2013
+from acoustics.standards import (iso_tr_25417_2007, 
+                                 iec_61672_1_2013,
+                                 iso_9613_1_1993,
+                                 )
