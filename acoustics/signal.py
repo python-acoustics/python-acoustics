ef30bd6c2f37544095ea257538a1252d2c3ee72e
Frederik Rietdijk <fridh@fridh.nl>
2014-12-13 11:27:31 +0100

    Mean square, root mean square and normalise

--- acoustics/signal.py
+++ acoustics/signal.py
@@ -384,18 +384,46 @@ class OctaveBand(Frequencies):
         
     def __repr__(self):
         return "OctaveBand({})".format(str(self.center))
+
+
+def ms(x):
+    """Mean value of signal `x` squared.
     
+    """
+    return (np.abs(x)**2.0).mean()
   
 def rms(x):
     """
-    Root mean square.
+    Root mean squared.
     
     :param x: Dynamic quantity.
     
     .. math:: x_{rms} = lim_{T \\to \\infty} \\sqrt{\\frac{1}{T} \int_0^T |f(x)|^2 \\mathrm{d} t }
     
+    :seealso: :func:`ms`.
+    
     """
-    return np.sqrt((np.abs(x)**2.0).mean())
+    return np.linalg.norm(x)
+    #return np.sqrt((np.abs(x)**2.0).mean())
+
+def normalise(y, x=None):
+    """Normalise power in y to a (standard normal) white noise signal.
+    
+    Optionally normalise to power in signal `x`.
+    
+    #The mean power of a Gaussian with :math:`\\mu=0` and :math:`\\sigma=1` is 1.
+    """
+    #return y * np.sqrt( (np.abs(x)**2.0).mean() / (np.abs(y)**2.0).mean() )
+    if x is not None:
+        x = ms(x)
+    else:
+        x = 1.0
+    return y * np.sqrt( x / ms(y) )
+    #return y * np.sqrt( 1.0 / (np.abs(y)**2.0).mean() )
+    
+    ## Broken? Caused correlation in auralizations....weird!
+    
+    
 
 
 def window_scaling_factor(window):
