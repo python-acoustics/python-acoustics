bcc22812841ef8fb9bf4e91cc4b27b64af736f3a
Frederik Rietdijk <fridh@fridh.nl>
2014-12-13 11:26:50 +0100

    Octave: rename keyword order to fraction
    It is meant to mean the fraction of the fractional-octave filter, and
    not the order of the filter.

--- acoustics/octave.py
+++ acoustics/octave.py
@@ -18,52 +18,52 @@ REFERENCE = 1000.0
 Reference frequency.
 """
 
-def band_of_frequency(f, order=1, ref=REFERENCE):
+def band_of_frequency(f, fraction=1, ref=REFERENCE):
     """
     Calculate the band ``n`` from a given center frequency.
 
     :param f: Frequency :math:`f`.
-    :param order: Band order.
+    :param fraction: Band fraction.
     :param ref: Reference center frequency :math:`f_0`.
     """
-    return np.round( ( np.log2(f/ref) - 1.0/order ) * order)
+    return np.round( ( np.log2(f/ref) - 1.0/fraction ) * fraction)
 
 
-def frequency_of_band(n, order=1, ref=REFERENCE):
+def frequency_of_band(n, fraction=1, ref=REFERENCE):
     """
     Calculate center frequency of band ``n``.
     
     :param n: band ``n``.
-    :param order: Order of octave.
+    :param fraction: Order of octave.
     :param ref: Reference center frequency.
     """
-    return ref * 10.0**(3.0/order/10.0) * 2.0**(n/order)
+    return ref * 10.0**(3.0/fraction/10.0) * 2.0**(n/fraction)
 
 
-def upper_frequency(center, order=1):
+def upper_frequency(center, fraction=1):
     """
-    Upper frequency of frequency band given a center frequency and order.
+    Upper frequency of frequency band given a center frequency and fraction.
     
     :param centr: Center frequencies.
-    :param order: Fraction of octave.
+    :param fraction: Fraction of octave.
     
     .. math:: f_u = f_c \cdot 2^{\\frac{+1}{2N}}
     
     """
-    return center * 2.0**(+1.0/(2.0*order))
+    return center * 2.0**(+1.0/(2.0*fraction))
     
     
-def lower_frequency(center, order=1):
+def lower_frequency(center, fraction=1):
     """
-    Lower frequency of frequency band given a center frequency and order.
+    Lower frequency of frequency band given a center frequency and fraction.
     
     :param center: Center frequencies.
-    :param order: Fraction of octave.
+    :param fraction: Fraction of octave.
     
     .. math:: f_l = f_c \cdot 2^{\\frac{-1}{2N}}
     
     """
-    return center * 2.0**(-1.0/(2.0*order))    
+    return center * 2.0**(-1.0/(2.0*fraction))    
 
 
 class Octave(object):
@@ -71,14 +71,14 @@ class Octave(object):
     Class to calculate octave center frequencies.
     """
 
-    def __init__(self, order=1, interval=None, fmin=None, fmax=None, unique=False, reference=REFERENCE):
+    def __init__(self, fraction=1, interval=None, fmin=None, fmax=None, unique=False, reference=REFERENCE):
         
         self.reference = reference
         """
         Reference center frequency :math:`f_{c,0}`.
         """
         
-        self.order = order
+        self.fraction = fraction
         """
         Fraction of octave.
         """
@@ -86,7 +86,7 @@ class Octave(object):
         if (interval is not None) and (fmin is not None or fmax is not None):
             raise AttributeError
         
-        self._interval = interval
+        self._interval = np.asarray(interval)
         """Interval"""
         
         self._fmin = fmin
@@ -139,7 +139,7 @@ class Octave(object):
         if self._fmin or self._fmax:
             pass
         else:
-            self._interval = x if isinstance(x, np.ndarray) else np.array(x)
+            self._interval = np.asarray(x)
         
     def _n(self, f):
         """
@@ -149,7 +149,7 @@ class Octave(object):
         
         See also :func:`band_of_frequency`.
         """
-        return band_of_frequency(f, order=self.order, ref=self.reference)
+        return band_of_frequency(f, fraction=self.fraction, ref=self.reference)
         
     def _fc(self, n):
         """
@@ -159,7 +159,7 @@ class Octave(object):
         
         See also :func:`frequency_of_band`.
         """
-        return frequency_of_band(n, order=self.order, ref=self.reference)
+        return frequency_of_band(n, fraction=self.fraction, ref=self.reference)
     
     @property
     def n(self):
@@ -201,7 +201,7 @@ class Octave(object):
         
         See also :func:`lower_frequency`.
         """
-        return lower_frequency(self.center, self.order)
+        return lower_frequency(self.center, self.fraction)
     
     @property
     def upper(self):
@@ -212,7 +212,7 @@ class Octave(object):
         
         See also :func:`upper_frequency`.
         """
-        return upper_frequency(self.center, self.order)
+        return upper_frequency(self.center, self.fraction)
         
     
 ###def center_frequency_octave(frequencies, order=1):
