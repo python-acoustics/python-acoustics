b4046bc823b52ead409ded5728b76ed5b26a1616
Frederik Rietdijk <fridh@fridh.nl>
2014-12-17 10:43:49 +0100

    Rewrite of iec 61672
    - fast and slow now only do the integration. fast_level and slow_level
    integrate and return a level
    - fix integrate. Conversion from zpk to tf was missing
    - improve integrate and average. Both work with at least 2D arrays now.

--- acoustics/standards/iec_61672_1_2013.py
+++ acoustics/standards/iec_61672_1_2013.py
@@ -15,6 +15,7 @@ kinds of sound measuring instruments [IEC61672]_:
 
 """
 import numpy as np
+from scipy.signal import zpk2tf
 from scipy.signal import lfilter, bilinear
 from math import floor
 from .iso_tr_25417_2007 import REFERENCE_PRESSURE
@@ -50,9 +51,17 @@ WEIGHTING_Z = np.array([0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0
 Frequency weighting Z.
 """
 
+FAST = 0.125
+"""FAST time-constant.
+"""
+
+SLOW = 1.000
+"""SLOW time-constant.
+"""
+
+
 def time_averaged_sound_level(pressure, sample_frequency, averaging_time, reference_pressure=REFERENCE_PRESSURE):
-    """
-    Time-averaged sound pressure level.
+    """Time-averaged sound pressure level.
     
     :param pressure: Dynamic pressure.
     :param sample_frequency: Sample frequency.
@@ -61,13 +70,12 @@ def time_averaged_sound_level(pressure, sample_frequency, averaging_time, refere
     
     """
     levels = 10.0 * np.log10( average(pressure**2.0, sample_frequency, averaging_time) / reference_pressure**2.0)
-    times = np.arange(len(levels)) * averaging_time
+    times = np.arange(level.shape[-1]) * averaging_time
     return times, levels
 
 
 def average(data, sample_frequency, averaging_time):
-    """
-    Average the sound pressure squared.
+    """Average the sound pressure squared.
     
     :param data: Energetic quantity, e.g. :math:`p^2`.
     :param sample_frequency: Sample frequency.
@@ -79,14 +87,19 @@ def average(data, sample_frequency, averaging_time):
     .. note:: Because :math:`f_s \\cdot t_i` is generally not an integer, samples are discarded. This results in a drift of samples for longer signals (e.g. 60 minutes at 44.1 kHz).
     
     """
-    n = int(floor(averaging_time * sample_frequency))
-    data = data[0:n*(len(data)//n)]
-    data = data.reshape((-1, n))
-    return data.mean(axis=1)
+    averaging_time = np.asarray(averaging_time)
+    sample_frequency = np.asarray(sample_frequency)
+    samples = data.shape[-1]
+    n = np.floor(averaging_time * sample_frequency).astype(int)
+    data = data[..., 0:n*(samples//n)] # Drop the tail of the signal.
+    newshape = list(data.shape[0:-1])
+    newshape.extend([-1, n])
+    data = data.reshape(newshape)
+    #data = data.reshape((-1, n))
+    return data.mean(axis=-1)
 
 def time_weighted_sound_level(pressure, sample_frequency, integration_time, reference_pressure=REFERENCE_PRESSURE):
-    """
-    Time-weighted sound pressure level.
+    """Time-weighted sound pressure level.
     
     :param pressure: Dynamic pressure.
     :param sample_frequency: Sample frequency.
@@ -94,12 +107,11 @@ def time_weighted_sound_level(pressure, sample_frequency, integration_time, refe
     :param reference_pressure: Reference pressure.
     """
     levels = 10.0 * np.log10( integrate(pressure**2.0, sample_frequency, integration_time) / reference_pressure**2.0)
-    times = np.arange(len(levels)) * integration_time
+    times = np.arange(levels.shape[-1]) * integration_time
     return times, levels
-    
+
 def integrate(data, sample_frequency, integration_time):
-    """
-    Integrate the sound pressure squared using exponential integration.
+    """Integrate the sound pressure squared using exponential integration.
     
     :param data: Energetic quantity, e.g. :math:`p^2`.
     :param sample_frequency: Sample frequency.
@@ -111,21 +123,63 @@ def integrate(data, sample_frequency, integration_time):
     .. note:: Because :math:`f_s \\cdot t_i` is generally not an integer, samples are discarded. This results in a drift of samples for longer signals (e.g. 60 minutes at 44.1 kHz).
     
     """
-    b, a = bilinear([1.0], [1.0, integration_time], fs=sample_frequency) # Bilinear: Analog to Digital filter.
-    n = int(floor(integration_time * sample_frequency))
-    data = data[0:n*(len(data)//n)]
-    data = data.reshape((-1, n)) # Divide in chunks over which to perform the integration.
-    return lfilter(b, a, data)[:,n-1] # Perform the integration. Select the final value of the integration.
-
+    integration_time = np.asarray(integration_time)
+    sample_frequency = np.asarray(sample_frequency)
+    samples = data.shape[-1]
+    b, a  = zpk2tf([1.0], [1.0, integration_time], [1.0])
+    b, a = bilinear(b, a, fs=sample_frequency)
+    #b, a = bilinear([1.0], [1.0, integration_time], fs=sample_frequency) # Bilinear: Analog to Digital filter.
+    n = np.floor(integration_time * sample_frequency).astype(int)
+    data = data[..., 0:n*(samples//n)]
+    newshape = list(data.shape[0:-1])
+    newshape.extend([-1, n])
+    data = data.reshape(newshape)
+    #data = data.reshape((-1, n)) # Divide in chunks over which to perform the integration.
+    return lfilter(b, a, data)[...,n-1] / integration_time # Perform the integration. Select the final value of the integration.
 
 def fast(data, fs):
+    """Apply fast (F) time-weighting.
+    
+    :param data: Energetic quantity, e.g. :math:`p^2`.
+    :param fs: Sample frequency.
+    
+    .. seealso:: :func:`integrate`
+    
     """
-    Apply fast (F) time-weighting.
-    """
-    return time_weighted_sound_level(data, fs, 0.125)
+    return integrate(data, fs, FAST)
+    #return time_weighted_sound_level(data, fs, FAST)
 
 def slow(data, fs):
+    """Apply slow (S) time-weighting.
+    
+    :param data: Energetic quantity, e.g. :math:`p^2`.
+    :param fs: Sample frequency.
+    
+    .. seealso:: :func:`integrate`
+    
     """
-    Apply slow (S) time-weighting.
+    return integrate(data, fs, SLOW)
+    #return time_weighted_sound_level(data, fs, SLOW)
+
+def fast_level(data, fs):
+    """Time-weighted (FAST) sound pressure level.
+    
+    :param data: Dynamic pressure.
+    :param fs: Sample frequency.
+    
+    .. seealso:: :func:`time_weighted_sound_level`
+    
     """
-    return time_weighted_sound_level(data, fs, 1.0)
+    return time_weighted_sound_level(data, fs, FAST)
+
+def slow_level(data, fs):
+    """Time-weighted (SLOW) sound pressure level.
+
+    :param data: Dynamic pressure.
+    :param fs: Sample frequency.
+    
+    .. seealso:: :func:`time_weighted_sound_level`
+    
+    """
+    return time_weighted_sound_level(data, fs, SLOW)
+
