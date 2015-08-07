eefdc650134c77c3a5f27c7489c941b88c6d8f3a
Frederik Rietdijk <fridh@fridh.nl>
2015-08-07 12:49:00 +0200

    Tests: remove __init__.py
    http://pytest.org/latest/goodpractises.html
    
    [ci skip]

--- tests/test_room.py
+++ tests/test_room.py
@@ -7,7 +7,10 @@ from acoustics.room import (mean_alpha, nrc, t60_sabine, t60_eyring,
                             t60_millington, t60_fitzroy, t60_arau,
                             t60_impulse, c50_from_file, c80_from_file)
 from acoustics.bands import octave, third
-from tests.get_data_path import data_path
+
+import sys
+sys.path.append('..')
+from get_data_path import data_path
 
 
 def setup_module(room):
