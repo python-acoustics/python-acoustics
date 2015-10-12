28003cd4a2f2f49c3a226f039e480096bf8e015a
Frederik Rietdijk <fridh@fridh.nl>
2015-10-12 19:43:25 +0200

    test: sound exposure level

---
+++ tests/standards/test_iso_tr_25417_2007.py
@@ -0,0 +1,20 @@
+
+
+from acoustics.standards.iso_tr_25417_2007 import *
+
+
+
+def test_sound_exposure_level():
+
+    duration = 20.0
+    fs = 44100.
+    samples = int(fs*duration)
+
+    signal = np.random.randn(samples)
+
+    leq = equivalent_sound_pressure_level(signal)
+    sel = sound_exposure_level(signal, fs)
+
+    assert ( (sel - leq) - 10.0*np.log10(duration) ) < 0.1
+
+
