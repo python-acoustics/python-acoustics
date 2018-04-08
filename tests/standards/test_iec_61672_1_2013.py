b7771f9784fabde85deb14d2d26c734276b3f28c
Frederik Rietdijk <fridh@fridh.nl>
2018-04-08 10:03:21 +0200

    fix indentation

--- tests/standards/test_iec_61672_1_2013.py
+++ tests/standards/test_iec_61672_1_2013.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from acoustics.standards.iec_61672_1_2013 import *
 from scipy.signal import freqresp
-   
+
 def signal_fs():
     fs = 4000.0
     f = 400.0
@@ -16,25 +16,25 @@ def signal_fs():
 
 def test_fast_level():
     """Test whether integration with time-constant FAST gives the correct level.
-    
+
     Note that the reference sound pressure is used.
-    
+
     In this test the amplitude of the sine is 1, which means the mean squared $MS$ is 0.5
     With a reference pressure $p_r$ of 2.0e-5 the level should be 91 decibel
-    
+
     .. math:: L = 10 \cdot \\log_{10}{\\left(\\frac{MS}{p_r^2} \\right)}
-    
+
     .. math:: L = 10 \cdot \\log_{10}{\\left(\\frac{0.5}{(2e-5)^2} \\right)} = 91
-    
+
     """
     x, fs = signal_fs()
 
     times, levels = fast_level(x, fs)
-    assert( abs(levels.mean() - 91 ) < 0.05 ) 
-    
+    assert( abs(levels.mean() - 91 ) < 0.05 )
+
     x *= 4.0
     times, levels = fast_level(x, fs)
-    assert( abs(levels.mean() - 103 ) < 0.05 ) 
+    assert( abs(levels.mean() - 103 ) < 0.05 )
 
 
 def test_slow_level():
@@ -43,40 +43,40 @@ def test_slow_level():
     x, fs = signal_fs()
 
     times, levels = fast_level(x, fs)
-    assert( abs(levels.mean() - 91 ) < 0.05 ) 
+    assert( abs(levels.mean() - 91 ) < 0.05 )
 
     x *= 4.0
     times, levels = fast_level(x, fs)
-    assert( abs(levels.mean() - 103 ) < 0.05 ) 
+    assert( abs(levels.mean() - 103 ) < 0.05 )
 
 
 def test_time_weighted_sound_level():
     x, fs = signal_fs()
     fast = 0.125
-    
+
     times, levels = time_weighted_sound_level(x, fs, fast)
-    assert( abs(levels.mean() - 91 ) < 0.05 ) 
-    
+    assert( abs(levels.mean() - 91 ) < 0.05 )
+
     x *= 4.0
     times, levels = time_weighted_sound_level(x, fs, fast)
-    assert( abs(levels.mean() - 103 ) < 0.05 ) 
+    assert( abs(levels.mean() - 103 ) < 0.05 )
 
 
 def test_time_averaged_sound_level():
     x, fs = signal_fs()
     fast = 0.125
-    
+
     times, levels = time_averaged_sound_level(x, fs, fast)
-    assert( abs(levels.mean() - 91 ) < 0.05 ) 
-    
+    assert( abs(levels.mean() - 91 ) < 0.05 )
+
     x *= 4.0
     times, levels = time_averaged_sound_level(x, fs, fast)
-    assert( abs(levels.mean() - 103 ) < 0.05 ) 
+    assert( abs(levels.mean() - 103 ) < 0.05 )
 
-    
+
 
 class TestWeighting():
-    
+
     @pytest.fixture(params=['A', 'C', 'Z'])
     def weighting(self, request):
         return request.param
@@ -86,7 +86,7 @@ class TestWeighting():
         values = WEIGHTING_VALUES[weighting]
         function_values = WEIGHTING_FUNCTIONS[weighting](frequencies)
         assert(np.abs(values-function_values).max() < 0.3)
-        
+
 
     def test_weighting_systems(self, weighting):
         frequencies = NOMINAL_THIRD_OCTAVE_CENTER_FREQUENCIES
@@ -94,5 +94,5 @@ class TestWeighting():
         w, H = freqresp((WEIGHTING_SYSTEMS[weighting]()), w=2.0*np.pi*frequencies)
         results = 20.0*np.log10(np.abs(H))
         assert(np.abs(values-results).max() < 0.3)
-            
-        
+
+
--- tests/standards/test_iso_1996_2_2007.py
+++ tests/standards/test_iso_1996_2_2007.py
@@ -10,15 +10,15 @@ def test_tonality():
     fs = 10025.0
     samples = int(fs*duration)
     times = np.arange(samples)/fs
-    
-    signal = Signal(np.sin(2.0*np.pi*1000.0*times), fs)  
+
+    signal = Signal(np.sin(2.0*np.pi*1000.0*times), fs)
 
     tonality = Tonality(signal, signal.fs)
-    
+
     # Test methods before analysis
     tonality.spectrum
     tonality.plot_spectrum()
-    
+
     tonality.frequency_resolution
     tonality.effective_analysis_bandwidth
 
@@ -26,30 +26,30 @@ def test_tonality():
     with pytest.raises(ValueError):
         print(tonality.overview())
     tonality.results_as_dataframe()
-    
+
     assert len(list(tonality.noise_pauses)) == 0
     assert len(list(tonality.tones)) == 0
     assert len(list(tonality.critical_bands)) == 0
-    
+
     # Perform analysis
     tonality.determine_noise_pauses().analyse()
-    
+
     assert len(list(tonality.noise_pauses)) == 1
     assert len(list(tonality.tones)) == 1
     assert len(list(tonality.critical_bands)) == 1
-    
+
     tonality.critical_band_at(900.0)
-    
+
     tonality.dominant_tone
     print(tonality.overview())
     tonality.results_as_dataframe()
     tonality.plot_results()
-    
 
 
-#Target = collections.namedtuple('Target', ['tonal_level', 
-                                           #'masking_noise_level', 
-                                           #'tonal_audibility', 
+
+#Target = collections.namedtuple('Target', ['tonal_level',
+                                           #'masking_noise_level',
+                                           #'tonal_audibility',
                                            #'adjustment'])
 #TARGET_DATA = {
     #'example1': Target(46.7, 37.3, 13.7, 6.0),
@@ -64,26 +64,26 @@ def test_tonality():
 
 #def test_verify_standard_examples(example):
     #"""Verify against the examples shown in the standard.
-    
-       
+
+
     #.. note:: The FTP server is slow. Be patient.
-    
+
     #For the examples in the standard:
     #- Number of spectra: 350
     #- Signal duration: 2 minutes
     #- Window: Hanning
     #- Averaging: Linear
     #- Effective analysis bandwidth: 4.39 Hz
-    
-    #The sample frequency is 44.1 kHz. Dividing this sample frequency by 
-    #results in 360 spectra and an effective analysis bandwidth of 
+
+    #The sample frequency is 44.1 kHz. Dividing this sample frequency by
+    #results in 360 spectra and an effective analysis bandwidth of
     #4.5 Hz, which is very close to the example.
 
     #"""
     #from ftplib import FTP
     #import tempfile
     #import os
-    
+
     #FOLDER = 'THP wave files/'
     #FILES = {'calibration' : 'cal. 93,8 dB.wav',
              #'example1': 'sam 03 eks 1.wav',
@@ -92,13 +92,13 @@ def test_tonality():
              #'example4': 'sam 03 eks 4.wav',
              #}
     #CHANNEL = 0
-    
+
     #fs = 44100.0
     #duration = 120.0
     #samples = duration*fs
     #spectra = 350
     #bins = samples // spectra
-    
+
     ## Obtain data from Delta
     ## https://noiselabdk.wordpress.com/demo-download/
     ##hostname = 'ftp.delta.dk'
@@ -112,18 +112,18 @@ def test_tonality():
         ##with open(local_data, 'wb') as target_file:
             ##ftp.retrbinary('RETR %s' % remote_data, target_file.write)
 
-    
+
 
     #calibration_at = 93.8 # decibel
     #signal = Signal.from_wav(os.path.join(FOLDER, FILES['calibration']))[CHANNEL]
     #calibration_factor = 10.0**((calibration_at - signal.leq())/20.0)
-    
+
     #target = TARGET_DATA[example]
     #filename = os.path.join(FOLDER, FILES[example])
-    
+
     #signal = Signal.from_wav(filename)[CHANNEL].pick(0.0, 60.0).weigh('A') * calibration_factor
     #tonality = Tonality(signal, signal.fs, bins=bins)
-    #tonality.determine_noise_pauses().analyse()        
+    #tonality.determine_noise_pauses().analyse()
 
     #cb = tonality.dominant_tone.critical_band
     #print(tonality.results())
@@ -131,4 +131,3 @@ def test_tonality():
     #assert abs(cb.masking_noise_level - target.masking_noise_level) < 1.0
     #assert abs(cb.tonal_audibility - target.tonal_audibility) < 1.0
     #assert abs(cb.adjustment - target.adjustment) < 1.0
-    
